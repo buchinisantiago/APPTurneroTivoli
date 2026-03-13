001ae69fafd7eebcdf5690e99bcd7483c47bc1c3
Cached10s <buchinisantiago@gmail.com>
2026-03-13 11:24:45 -0300

    fix(db): set emulate prepares to true for supbase pooler compatibility

--- db.php
+++ db.php
@@ -42,7 +42,7 @@ function getDB(): PDO
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_EMULATE_PREPARES => true, // Required for Supabase Connection Pooler (PgBouncer)
                 ]
             );
         } catch (PDOException $e) {
